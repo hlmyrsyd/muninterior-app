23956311a0f99ee58f9c1d728922bcf78c96a4b3
jodyboy <elite.crew66@gmail.com>
2024-05-02 17:04:42 +0700

    Header on Progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,7 @@ function Header() {
     return (
         <section id='home' className="hero">
             <div className="hero-text">
-                <h1>Welcome to Mun Interior</h1>
-                <p>The joy and excitement of creating custom furniture is what drives us. A home is not only filled with furniture, but also with lifestyle and memories. Bertungku envisions a thoughtful living space with the hope that precious and valuable families will create a lot of history together.</p>
+                <h1>Bespoke Interior Design & 3D Visual</h1>
                 <a href="#portfolio" className="btn">Check Our Work</a>
             </div>
         </section>
