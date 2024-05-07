47d70648444eeadb8a1fcd4fdc13599dac2c5c53
jodyboy <elite.crew66@gmail.com>
2024-05-07 17:15:09 +0700

    nav and hero update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import './Header.css'
 function Header() {
     
     return (
-        <section id='home' className="hero">
+        <div id='home' className="hero">
             <div className="hero-text">
-                <h1>Bespoke Interior Design & 3D Visual</h1>
+                <h1>Bespoke Interior Design <br/>and 3D Visual</h1>
                 <a href="#portfolio" className="btn">Check Our Work</a>
             </div>
-        </section>
+        </div>
     );
 }
 
